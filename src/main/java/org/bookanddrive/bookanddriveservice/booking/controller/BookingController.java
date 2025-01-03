8d92aadf3534ccd52f2d6cd2f6c93e848078ce25
Jakub Piecuch <kuba.piecuch.kp@gmail.com>
2025-01-03 13:58:47 +0100

    feat: PLAT-0000 add booking controller

---
+++ src/main/java/org/bookanddrive/bookanddriveservice/booking/controller/BookingController.java
@@ -0,0 +1,7 @@
+package org.bookanddrive.bookanddriveservice.booking.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookingController {
+}
