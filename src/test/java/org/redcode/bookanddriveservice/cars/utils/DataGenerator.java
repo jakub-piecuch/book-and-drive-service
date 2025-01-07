ad1277e9a142d2e9402d6b5efe816a96c83eaf77
KUBA <kuba.piecuch.kp@gmail.com>
2025-01-07 22:29:49 +0100

    feat: PLAT-0000 add git workflows and UT

--- src/test/java/org/redcode/bookanddriveservice/cars/utils/DataGenerator.java
+++ src/test/java/org/redcode/bookanddriveservice/cars/utils/DataGenerator.java
@@ -8,23 +8,26 @@ import org.redcode.bookanddriveservice.cars.model.CarEntity;
 @UtilityClass
 public class DataGenerator {
 
-    private final UUID CAR_ID = UUID.randomUUID();
+    private static final String MAKE = "Toyota";
+    private static final UUID CAR_ID = UUID.randomUUID();
+    private static final String MODEL = "Corolla";
+    public static final String REGISTRATION_NUMBER = "ASDQW123";
 
     public static Car generateCar() {
         return Car.builder()
             .id(CAR_ID)
-            .make("Toyota")
-            .model("Corolla")
-            .registrationNumber("ASDQW123")
+            .make(MAKE)
+            .model(MODEL)
+            .registrationNumber(REGISTRATION_NUMBER)
             .build();
     }
 
     public static CarEntity generateCarEntity() {
         return CarEntity.builder()
             .id(CAR_ID)
-            .make("Toyota")
-            .model("Corolla")
-            .registrationNumber("ASDQW123")
+            .make(MAKE)
+            .model(MODEL)
+            .registrationNumber(REGISTRATION_NUMBER)
             .build();
     }
 }
